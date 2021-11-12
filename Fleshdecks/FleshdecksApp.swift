cdc8223fbd69fee2e03fc143f45ebb99ceeef59f
DeLucaAntonio <tonidl@hotmail.it>
2021-11-12 11:48:35 +0100

    Initial Commit

---
+++ Fleshdecks/FleshdecksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FleshdecksApp.swift
+//  Fleshdecks
+//
+//  Created by Antonio on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FleshdecksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
