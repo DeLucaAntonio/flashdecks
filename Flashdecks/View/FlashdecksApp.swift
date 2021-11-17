be4d223da64977fb679342725f3857c53b2590dc
Felice <dipalmafelice1992@gmail.com>
2021-11-17 12:17:55 +0100

    First

---
+++ Flashdecks/View/FlashdecksApp.swift
@@ -0,0 +1,20 @@
+//
+//  FlashdecksApp.swift
+//  Flashdecks
+//
+//  Created by Antonio on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashdecksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            //AverageDuration(hours: "1", minutes: "40", seconds: "35")
+            Home()
+            //SessionCompleted(sessionCompleted: "29")
+           // TotalSuccess(succes: "70")
+        }
+    }
+}
