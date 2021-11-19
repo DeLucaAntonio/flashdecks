f371adc314ce4dbac4fa12ae4d56bca52170687e
frankkk1013 <59925828+frankkk1013@users.noreply.github.com>
2021-11-19 09:41:40 +0100

    aggiunta view flashcard

---
+++ Flashdecks/Flashcard.swift
@@ -0,0 +1,20 @@
+//
+//  Flashcard.swift
+//  Flashdecks
+//
+//  Created by Francesco Iaccarino on 17/11/21.
+//
+
+import SwiftUI
+
+struct Flashcard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Flashcard_Previews: PreviewProvider {
+    static var previews: some View {
+        Flashcard()
+    }
+}
